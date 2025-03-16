5e25a9b8632fb6329469424261fd0baf7b935605
matheusknaul <contatomatheush0909@gmail.com>
2025-03-16 01:20:55 -0300

    feat: Added health component features

--- src/main/java/com/matheusknaul/GameEngine/components/HealthComponent.java
+++ src/main/java/com/matheusknaul/GameEngine/components/HealthComponent.java
@@ -1,5 +1,36 @@
 package com.matheusknaul.GameEngine.components;
 
 public class HealthComponent {
-
+	private float totalLife;
+	private float life;
+	
+	public HealthComponent(float life) {
+		this.life = life;
+	}
+	
+	public float getLife() {
+		return this.life;
+	}
+	
+	public float geTotaltLife() {
+		return this.totalLife;
+	}
+	
+	public void setTotalLife(float totalLife) {
+		this.totalLife = totalLife;
+	}
+	
+	public void setLife(float life) {
+		this.life = life;
+	}
+	
+	public void loseHealth(float rawDamage, float armor) {
+		float damage = rawDamage - armor;
+		setLife(getLife() - damage);
+	}
+	
+	public void gainHealth(float cure) {
+		setLife(getLife() + cure);
+	}
+	
 }
