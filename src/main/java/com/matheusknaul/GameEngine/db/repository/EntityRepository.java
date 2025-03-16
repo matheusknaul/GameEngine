b85b8a8c98b16ec84a57c49176e21a1cefee3df6
matheusknaul <contatomatheush0909@gmail.com>
2025-03-16 02:37:41 -0300

    feat: Create EntityRepository

--- src/main/java/com/matheusknaul/GameEngine/db/repository/EntityRepository.java
+++ src/main/java/com/matheusknaul/GameEngine/db/repository/EntityRepository.java
@@ -1,5 +1,34 @@
 package com.matheusknaul.GameEngine.db.repository;
 
-public interface EntityRepository{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import com.matheusknaul.GameEngine.core.Entity;
+import com.matheusknaul.GameEngine.db.persistence.Repository;
+
+public class EntityRepository implements Repository<Entity>{
+	private final Connection connection;
+	
+	public EntityRepository(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public void save(Entity entity) {
+		try {
+			String query = "INSERT INTO entities (name) VALUES (?)";
+			try(PreparedStatement stmt = connection.prepareStatement(query)){
+				stmt.setString(1, entity.getName());
+				stmt.executeUpdate();
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteById(int id) {
+		try {
+			String query = "DELETE FROM entities WHERE id = ?";
+		}
+	}
 }
