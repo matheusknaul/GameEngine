9d6d9c1fea37d4df5fdf52b9634b3077d366d959
matheusknaul <contatomatheush0909@gmail.com>
2025-03-16 19:47:17 -0300

    feat: Create Game Panel

--- src/main/java/com/matheusknaul/GameEngine/App.java
+++ src/main/java/com/matheusknaul/GameEngine/App.java
@@ -1,13 +1,26 @@
 package com.matheusknaul.GameEngine;
 
-import com.matheusknaul.GameEngine.gui.SystemGUI;
+import javax.swing.JFrame;
 
-/**
- * Hello world!
- */
+import com.matheusknaul.GameEngine.gui.GamePanel;
+
 public class App {
     public static void main(String[] args) {
-        SystemGUI sg = new SystemGUI();
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+        
+        GamePanel gamePanel = new GamePanel();
+        gamePanel.run();
+        window.add(gamePanel);
+        
+        window.pack();
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+        gamePanel.startGameThread();
     }
     
 }
